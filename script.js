6d3c9cfc4372bfa1ea2fdd182f4c9f637b697073
Kaif Kazi <Kaifkazi033@gmail.com>
2023-12-27 11:31:13 +0530

    JS completed

---
+++ script.js
@@ -0,0 +1,24 @@
+function appendToResult(value) {
+  if (value === "AC") {
+    clearResult();
+  } else if (value === "DEL") {
+    var currentValue = document.getElementById("result").value;
+    document.getElementById("result").value = currentValue.slice(0, -1);
+  } else {
+    document.getElementById("result").value += value;
+  }
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculateResult() {
+  try {
+    document.getElementById("result").value = eval(
+      document.getElementById("result").value
+    );
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+  }
+}
